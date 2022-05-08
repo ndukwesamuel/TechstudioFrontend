c0b895a5e6d6c4cac9296c7095c6a1d2459d988a
ndukwesamuel <ndukwesamuel23@yahoo.com>
2022-05-08 06:28:36 +0100

    sjdsdj

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import Navbar from "./Components/Navigation/Navbar";
-import Home from "./Page/Home";
+import LearningTrack from "./Page/General/LearningTrack";
 
 function App() {
   return (
     <BrowserRouter>
       <section className="main_dashboard">
-        <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<LearningTrack />} />
           <Route path="class" element={<div> School</div>} />
           <Route path="message" element={<div> message</div>} />
           <Route path="task" element={<div> TAsk</div>} />
